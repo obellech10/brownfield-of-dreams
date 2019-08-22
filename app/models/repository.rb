4149d43f9be9d388867cba6c01bc77ee91b64e35
Andrew Johnson <manilda7@gmail.com>
2019-08-22 13:33:14 -0600

    Adds repository model and passes tests
    Co-Authored-By: Oscar Bellechassc  <obellech10@gmail.com>

---
+++ app/models/repository.rb
@@ -0,0 +1,9 @@
+class Repository
+  attr_reader :name,
+              :html_url
+
+  def initialize(attributes = {})
+    @name     = attributes[:name]
+    @html_url = attributes[:html_url]
+  end
+end

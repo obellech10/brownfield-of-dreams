c7ddb7dcb63e0fbf24fe4b6fffb755e8f769a46d
Andrew Johnson <manilda7@gmail.com>
2019-08-22 14:07:04 -0600

    Adds webmock in place of vcr for user_can_see_a_list_of_github_repos spec and fixture directory
    Co-Authored-By: Oscar Bellechasse <obellech10@gmail.com>

--- spec/features/user/user_can_see_a_list_of_github_repos_spec.rb
+++ spec/features/user/user_can_see_a_list_of_github_repos_spec.rb
@@ -3,22 +3,20 @@ require 'rails_helper'
 RSpec.describe "As a logged in user" do
   describe "When I visit /dashboard, I should see a section for 'Github'" do
     it "Under 'Github' I should see a list of 5 repositories with the name of each Repo linking to the repo on Github" do
-      VCR.use_cassette('github_auth', record: :new_episodes) do
+      json_response = File.open("./fixtures/github_repos.json")
+      stub_request(:get, "https://api.github.com/user/repos")
+        .to_return(status: 200, body: json_response)
 
-        user = create(:user)
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/dashboard'
 
-        visit '/dashboard'
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Github")
 
-        expect(current_path).to eq(dashboard_path)
-        expect(page).to have_content("Github")
-
-        within(first(".repo")) do
-          expect(page).to have_css(".name")
-        end
-        save_and_open_page
-
+      within(first(".repo")) do
+        expect(page).to have_css(".name")
       end
     end
   end

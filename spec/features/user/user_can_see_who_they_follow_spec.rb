409ea29eabc8130349558d7c7ebbf4f684409491
Oscar Bellechasse <obellech10@gmail.com>
2019-08-24 17:29:33 -0600

    Update user on test to have a github token
    Co-Authored-By: Andrew Johnson <manilda7@gmail.com>

--- spec/features/user/user_can_see_who_they_follow_spec.rb
+++ spec/features/user/user_can_see_who_they_follow_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-feature "User can see a list of users they follow" do
-  context "As a logged in user" do
+RSpec.describe "User can see a list of users they follow" do
+  describe "As a logged in user" do
     context "When I visit '/dashboard', I see a section for 'Github', under that I see a section 'Following'" do
       it "I should see a list of users I follow with their handles linking to their Github profile" do
         json_response = File.open("./fixtures/github_following.json")
         stub_request(:get, "https://api.github.com/user/following")
           .to_return(status: 200, body: json_response)
 
-        user = create(:user)
+        user = User.create!(first_name: 'Dan', last_name: 'Mariano', password: 'test', github_token: 'test', email: 'dan@example.com')
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
         visit '/dashboard'

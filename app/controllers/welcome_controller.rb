25ee36b184bfb59a0cb145d15df15824a892f036
Oscar Bellechasse <obellech10@gmail.com>
2019-08-25 13:25:08 -0600

    Update index method to include conditional on current user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
+    if current_user
+      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5) if params[:tag]
       @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+    else
+      @tutorials = Tutorial.non_classroom_tutorials.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5) if params[:tag]
+      @tutorials = Tutorial.non_classroom_tutorials.all.paginate(:page => params[:page], :per_page => 5)
     end
   end
 end

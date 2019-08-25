c606bfe9568c29783798da7b831b841b4b07f010
Oscar Bellechasse <obellech10@gmail.com>
2019-08-25 13:25:37 -0600

    Add class method test for non classroom tutorials

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'class methods' do
+    it 'Should only return non classroom tutorials' do
+      tutorial1 = create(:tutorial)
+      tutorial2 = Tutorial.create!(title: "test", description: 'test', classroom: true)
+      tutorial3 = create(:tutorial)
+
+      expect(Tutorial.non_classroom_tutorials.first).to eq(tutorial1)
+    end
+  end
 end

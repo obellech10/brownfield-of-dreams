70efabf8ecc9f381e9a0a5274289947e12cd74d7
Oscar Bellechasse <obellech10@gmail.com>
2019-08-25 13:26:28 -0600

    Update feature test for non classroom tutorials

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -22,5 +22,23 @@ describe 'Visitor' do
         expect(page).to have_content(tutorial1.description)
       end
     end
+
+    it 'can only see a list non classroom tutorials' do
+      tutorial1 = create(:tutorial)
+      tutorial2 = Tutorial.create!(title: "test", description: 'test', classroom: true)
+      tutorial3 = create(:tutorial)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial3.id)
+
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 2)
+
+      expect(page).to_not have_content(tutorial2.title)
+      expect(page).to_not have_content(tutorial2.description)
+    end
   end
 end

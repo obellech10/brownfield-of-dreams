12400de8d8dc169a6f4541f631cbf53087ef5ad0
Oscar Bellechasse <obellech10@gmail.com>
2019-08-28 23:23:00 -0600

    Update create action on admin controller

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -4,6 +4,14 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   def create
+    @tutorial = Tutorial.new(tutorial_params)
+    if @tutorial.save
+      flash[:notice] = "Successfully created tutorial"
+      redirect_to tutorial_path(@tutorial.id)
+    else
+      flash[:notice] = "Missing information, review form and resubmit"
+      redirect_to new_admin_tutorial_path
+    end
   end
 
   def new
@@ -20,6 +28,6 @@ class Admin::TutorialsController < Admin::BaseController
 
   private
   def tutorial_params
-    params.require(:tutorial).permit(:tag_list)
+    params.require(:tutorial).permit(:tag_list, :title, :description, :thumbnail)
   end
 end

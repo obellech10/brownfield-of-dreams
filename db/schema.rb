1369a84d8bea9046d90e97f3308eb022ab28e0f4
Andrew Johnson <manilda7@gmail.com>
2019-08-28 18:37:38 -0600

    Adds active column to Users table
    Co-Authored-By: Oscar Bellechasse <obellech10@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_194126) do
+ActiveRecord::Schema.define(version: 2019_08_27_204036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_194126) do
     t.datetime "updated_at", null: false
     t.string "github_token"
     t.string "github_nickname"
+    t.boolean "active", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 

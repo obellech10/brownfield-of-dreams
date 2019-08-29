94b63eb2fc393207b57e720d7160599bfb35237a
Andrew Johnson <manilda7@gmail.com>
2019-08-29 10:42:43 -0600

    Adds test to delete tutorial spec

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Tutorial, type: :model do
       expect(Tutorial.non_classroom_tutorials.first).to eq(tutorial1)
     end
   end
+
+  describe 'CRUD' do
+    it 'can be delete and a video associated with is deleted' do
+      tutorial_1 = Tutorial.create(id: 1)
+      video = Video.create(id: 1, tutorial_id: 1)
+
+      tutorial_1.destroy
+
+      expect(tutorial_1.destroyed?).to eq(true)
+      expect(Video.where(tutorial_id: tutorial_1.id)).to eq([])
+    end
+  end
 end
